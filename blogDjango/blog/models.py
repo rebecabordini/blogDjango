78c6cce348b898b04e72e8219dd8a233bd82d879
rebecabordini <bordini.rebeca@gmail.com>
2014-09-29 17:45:39 -0300

    Inserindo as views e o roteamentos das urls para as views, sem os templates

--- blogDjango/blog/models.py
+++ blogDjango/blog/models.py
@@ -5,9 +5,23 @@ class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
-    posted = models.DateField(db_index=True, auto_now_add=True)
+    posted = models.DateField(db_index=True)
     category = models.ForeignKey('blog.Category')
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_post', None, { 'slug': self.slug })
 
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_category', None, { 'slug': self.slug })
--- blogDjango/blogDjango/urls.py
+++ blogDjango/blogDjango/urls.py
@@ -4,4 +4,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+	url(r'^blog/view/(?P<slug>[^\.]+).html',  'blog.views.view_post', name='view_blog_post'),
+	url(r'^blog/category/(?P<slug>[^\.]+).html', 'blog.views.view_category', name='view_blog_category'),
 )
