2bf827fdbd9375c8ff4ed488ecd29ef39bacc7c5
rebecabordini <bordini.rebeca@gmail.com>
2014-09-29 15:49:06 -0300

    Iniciando o projeto Django, instalando app blog e inserindo os modelos de Blog e categoria

---
+++ blogDjango/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    posted = models.DateField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey('blog.Category')
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
