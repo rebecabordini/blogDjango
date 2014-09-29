32c3b06109d202b2abe4ad8f707a18fdf77b8cf9
rebecabordini <bordini.rebeca@gmail.com>
2014-09-29 15:58:51 -0300

    Adicionando o slug da categoria e do blog

--- blogDjango/blog/models.py
+++ blogDjango/blog/models.py
@@ -7,6 +7,21 @@ class Blog(models.Model):
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('blog.Category')
 
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_post', None, { 'slug': self.slug })
+
+
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('view_blog_category', None, { 'slug': self.slug })
