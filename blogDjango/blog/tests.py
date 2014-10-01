c07d1b2e74fc1e165bed877c0afe539e9a9382a4
rebecabordini <bordini.rebeca@gmail.com>
2014-10-01 17:57:33 -0300

    adicionando setUp teste testApenasPostsNoPassadoSaoExibidos

--- blogDjango/blog/tests.py
+++ blogDjango/blog/tests.py
@@ -1,10 +1,18 @@
 import unittest
+import datetime
 from blog.models import Blog, Category
-from blog.views import index
+
 
 class ValidacoesPost(unittest.TestCase):
 
-	def test_teste(self):
+	def setUp(self):
+		Blog.objects.all().delete()
+		Category.objects.all().delete()
+		categoria = Category.objects.create(title='Artes', slug='artes')
+		Blog.objects.create(title='BlogTestSucesso',slug='blogtestesucesso',body='Teste',posted=datetime.datetime.now().date(),category=categoria)
+		Blog.objects.create(title='BlogTestFalha',slug='blogtestefalha',body='Teste',posted=datetime.datetime.now().date(),category=categoria)
+
+	def testApenasPostsNoPassadoSaoExibidos(self):
 		self.assertEqual(1,1)
 
 if __name__ == '__main__':
