3e4e572feed4302d04b80c369568e1813e142917
rebecabordini <bordini.rebeca@gmail.com>
2014-10-03 14:42:06 -0300

    Modificando setUp da classe de teste para setUpClass

--- blogDjango/blog/tests.py
+++ blogDjango/blog/tests.py
@@ -6,13 +6,15 @@ from blog import views
 
 class ValidacoesPost(unittest.TestCase):
 
-	def setUp(self):
+	@classmethod
+	def setUpClass(self):
 		Blog.objects.all().delete()
 		Category.objects.all().delete()
 		categoria = Category.objects.create(title='Artes', slug='artes')
 		dataFutura = datetime.datetime.now().date() + datetime.timedelta(days=1)
 		Blog.objects.create(title='BlogTestSucesso',slug='blogtestesucesso',body='Teste',posted=datetime.datetime.now().date(),category=categoria)
 		Blog.objects.create(title='BlogTestFalha',slug='blogtestefalha',body='Teste',posted=dataFutura,category=categoria)
+		print "1"
 
 	def testApenasPostsNoPassadoSaoExibidos(self):
 		# posts = []
